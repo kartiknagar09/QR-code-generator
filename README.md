90da06694be0fa30ff8be132be81f153652f1f72
Kartik <nagarkartik82@gmail.com>
2023-02-11 22:45:16 +0530

    Edit readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # QR-code-generator
+
+QR Code Generator api:- https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=
+
+Output:- https://codepen.io/kartiknagar09/pen/mdjNjej
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function generateQR(){
 
     if(qrtext.value.length > 0){
 
-        qrimg.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+        qrimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
     
         imgbox.classList.add("show-img");
     }
